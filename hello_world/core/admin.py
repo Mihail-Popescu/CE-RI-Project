5efc76e50b7085a4cd411eef2fdfe5ec60fbda22
Mihail-Popescu <129216063+Mihail-Popescu@users.noreply.github.com>
2023-11-03 10:59:50 +0000

    commit7

--- hello_world/core/admin.py
+++ hello_world/core/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    actions = ['custom_redirect_action']
+
+    def custom_redirect_action(self, request, queryset):
+        # Implement your custom action logic here.
+        # For example, let's redirect to the "Add" page in the admin.
+        url = reverse('product-list')
+        return HttpResponseRedirect(url)
+
+    custom_redirect_action.short_description = "Redirect to Other Page"
+
+admin.site.register(Product, ProductAdmin)
